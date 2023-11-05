b64436cac78151ff4d24019de2da458658b7b8aa
Satyam Pandey <144729455+coalsea@users.noreply.github.com>
2023-11-05 04:37:48 -0500

    Add files via upload

---
+++ RedisData/pages/index.js
@@ -0,0 +1,12 @@
+import CarForm from '../lib/CarForm';
+import SearchForm from '../lib/SearchForm';
+
+export default function Home(props) {
+  return (
+    <main>
+      <h1>Create a Ship</h1>
+      <CarForm />
+      <hr />
+    </main>
+  );
+}
